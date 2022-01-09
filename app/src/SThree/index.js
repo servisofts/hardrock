2f992005d829f8e7a93ea96a06b55a244c51b491
rickypazd <35882906+rickypazd@users.noreply.github.com>
2022-01-09 18:43:53 -0400

    app

--- app/src/SThree/index.js
+++ app/src/SThree/index.js
@@ -3,25 +3,34 @@ import { connect } from 'react-redux';
 import { WebView } from 'react-native-webview'
 import { SText, SView } from 'servisofts-component';
 
-export default class TD extends Component {
+export default class SThree extends Component {
     constructor(props) {
         super(props);
         this.state = {
         };
     }
-    send(obj){
+    send(obj) {
         this.webview.postMessage(JSON.stringify(obj));
     }
     render() {
         return (
             <SView col={"xs-12"} height>
                 <WebView
-                    ref={(ref) => { this.webview = ref; }}
+                    ref={(ref) => {
+                        this.webview = ref;
+                    }}
                     style={{ width: "100%", backgroundColor: 'transparent', }}
                     originWhitelist={['*']}
-                    source={require("../../Components/Cubo/index.html")}
-                    renderLoading={() => {
-
+                    source={require("./index.html")}
+                    javaScriptEnabled={true}
+                    // injectedJavaScript={require("./funcions.wbjs").default}
+                    onLoadEnd={() => {
+                        // this.send({
+                        //     component: "init",
+                        //     data: {
+                        //         img1: "./ts1.jpg",
+                        //     }
+                        // });
                     }}
                     onMessage={(data) => {
                         var data = data.nativeEvent.data;
