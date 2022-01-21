6819ca97d95d37085f2cf920501cf1e5f49d201e
rickypazd <35882906+rickypazd@users.noreply.github.com>
2022-01-21 02:04:43 -0400

    as

--- app/src/DWV/web/src/Test/Blender.js
+++ app/src/DWV/web/src/Test/Blender.js
@@ -1,30 +1,33 @@
 import TEST from "./test.json";
 
 export default class Blender {
-    
+
     constructor(ThreeScene) {
         this.ThreeScene = ThreeScene;
         this.createTest();
     }
-    createTest () {
+    createTest() {
         const INSTANCE = this.ThreeScene;
 
         // loader.load("./test.gltf", function (gltf) {
-        this.ThreeScene.gltfLoader.parse(JSON.stringify(TEST), '',  async (gltf) => {
+        this.ThreeScene.gltfLoader.parse(JSON.stringify(TEST), '', async (gltf) => {
             INSTANCE.test = gltf.scene;
             INSTANCE.scene.add(INSTANCE.test);
-
+            gltf.scene.traverse(function (model) {
+                if (model.isMesh && model.name.includes("Guitarra")) {
+                    model.castShadow = true;
+                }
+            });
             INSTANCE.camera = INSTANCE.scene.getObjectByName("Camera").children[0];
             INSTANCE.piso = INSTANCE.scene.getObjectByName("Piso");
             INSTANCE.piso.receiveShadow = true;
+            // INSTANCE.piso.castShadow = true;
 
-            INSTANCE.cubo = INSTANCE.scene.getObjectByName("Guitarra");
-            // INSTANCE.cubo.receiveShadow = true;
-            INSTANCE.cubo.castShadow = true;
+            INSTANCE.guitarra = INSTANCE.scene.getObjectByName("Guitarra");
 
             INSTANCE.ventana = INSTANCE.scene.getObjectByName("Ventana");
             INSTANCE.ventana.receiveShadow = true;
-            // INSTANCE.ventana.castShadow = true;
+            INSTANCE.ventana.castShadow = true;
 
             INSTANCE.mixer.actions = [];
             for (var i = 0; i < gltf.animations.length; i++) {
@@ -32,7 +35,7 @@ export default class Blender {
                 INSTANCE.mixer.actions.push(action);
             }
             INSTANCE.mixers.push(INSTANCE.mixer);
-            
+
             INSTANCE.mixer.actions[0].setLoop(INSTANCE.LoopOnce);
             INSTANCE.mixer.actions[0].clampWhenFinished = true;
 
@@ -40,7 +43,7 @@ export default class Blender {
 
             INSTANCE.light = INSTANCE.scene.getObjectByName("Light").children[0];
             INSTANCE.light.castShadow = true;
- 
+
 
 
         }, undefined, function (error) {
